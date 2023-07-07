9b9fe60280b17eca5cdfafd148f6e4e32e7eecf0
Gaurangi Puradkar <gaurangifb@gmail.com>
2023-07-08 01:58:45 +0530

    carousel first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css"
+import Carousel from './components/Carousel'
+import {slides} from "./data/carouselData.json"
+
+const App = () => {
+  return (
+    <div className="App">
+      <Carousel data={slides} />
+    </div>
+  )
+}
+
+export default App
